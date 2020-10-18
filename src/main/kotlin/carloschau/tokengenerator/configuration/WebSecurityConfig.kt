c853cafdebce731bcbbc9a749989f5acb025094c
Carlos Chau <carlos.chau719@gmail.com>
2020-10-18 12:53:17 +0800

    add cors config

--- src/main/kotlin/carloschau/tokengenerator/configuration/WebSecurityConfig.kt
+++ src/main/kotlin/carloschau/tokengenerator/configuration/WebSecurityConfig.kt
@@ -5,7 +5,6 @@ import carloschau.tokengenerator.security.TokenAuthenticationFilter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -14,6 +13,10 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedGrantedAuthoritiesUserDetailsService
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +24,9 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Value("\${spring.security.web.ignoring.path}")
     lateinit var ignoringPath : Array<String>
+
+    @Value("\${spring.security.web.cors.origin}")
+    lateinit var corsOrigin : List<String>
 
     override fun configure(web: WebSecurity?) {
         web?.let {
@@ -30,13 +36,13 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         http?.let {
+            http.cors().configurationSource(corsConfigurationSource())
             http.csrf().disable()
             http.sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
             http.formLogin().disable()
             http.logout().disable()
-
             http.addFilter(getTokenAuthenticationFilter())
                     .authenticationProvider(getPreAuthenticatedAuthenticationProvider())
                     .authorizeRequests()
@@ -74,6 +80,20 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     @Bean
     fun getPreAuthenticationDetailsSource(): PreAuthenticationDetailsSource{
         return PreAuthenticationDetailsSource()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = corsOrigin
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("authorization", "content-type", "x-auth-token")
+        //configuration.allowedHeaders = listOf("*")
+        configuration.exposedHeaders = listOf("x-auth-token")
+        configuration.allowCredentials = true;
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
 
